c0e45173ad672c380b73eed631ff870bbb0248ea
Saebom <spring180@naver.com>
2025-05-29 17:14:02 +0900

    init: 폴더 구조 설정 (#1)

---
+++ src/components/common/index.ts
@@ -0,0 +1,13 @@
+// 폴더 내 index.ts 파일을 사용하여 하위 컴포넌트를 일괄 export
+// export { default as Button } from './Button';
+// export { default as Icon } from "./Icon";
+
+/*
+✨ 기존 방식
+import Button from "@/components/common/Button";
+import Icon from "@/components/common/Icon";
+
+✨index 파일 사용 시
+
+import { Button, Icon } from "@/components/common";
+*/
