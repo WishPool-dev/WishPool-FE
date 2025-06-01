3a4733f782dcb9d5519cae92422e76290e5ba9c4
Saebom <spring180@naver.com>
2025-06-01 16:16:36 +0900

    chore: style 파일 폴더 변경 (#9)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import './globals.css';
+import '@/styles/globals.css';
+
 import ReactQueryProvider from '@/components/ReactQueryProvider';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'WishPooL',
@@ -26,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
