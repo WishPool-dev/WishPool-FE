a322c411198e298b10105273e89d4b837bf681ee
봄 <saebom@Saebomui-MacBookPro.local>
2025-06-17 21:41:54 +0900

    feat: photo, gift 아이콘 svg 추가 (#18)

--- src/assets/icons/iconMap.ts
+++ src/assets/icons/iconMap.ts
@@ -2,8 +2,10 @@
 
 import BackIcon from '@/assets/icons/svg/icon_back.svg';
 import CancelIcon from '@/assets/icons/svg/icon_cancel.svg';
+import giftIcon from '@/assets/icons/svg/icon_gift.svg';
 import HomeIcon from '@/assets/icons/svg/icon_home.svg';
 import MenuIcon from '@/assets/icons/svg/icon_menu.svg';
+import photoIcon from '@/assets/icons/svg/icon_photo.svg';
 import WishIcon from '@/assets/icons/svg/icon_wish.svg';
 
 export const iconMap = {
@@ -12,4 +14,6 @@ export const iconMap = {
   menu: MenuIcon,
   wish: WishIcon,
   cancel: CancelIcon,
+  gift: giftIcon,
+  photoIcon: photoIcon,
 } as const;
