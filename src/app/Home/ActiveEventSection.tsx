11040ee94ce11edc65f887864e8b7d13634b1521
봄 <saebom@Saebomui-MacBookPro.local>
2025-06-17 21:37:49 +0900

    feat: 태그로 틀  생성 (#18)

--- src/app/Home/ActiveEventSection.tsx
+++ src/app/Home/ActiveEventSection.tsx
@@ -2,8 +2,21 @@
 
 import type { PlanType } from '@/types/planType';
 
-const ActiveEventSection = (planType: PlanType) => {
-  return <div>{planType}</div>;
+const CTA_MESSAGE = {
+  funding: '받고 싶은 선물로 나의 생일을 축하받고 싶다면?',
+  wishpool: '생일자가 받고 싶은 선물이 무엇인지 알고 싶다면?',
+};
+
+const ActiveEventSection = ({ planType }: { planType: PlanType }) => {
+  return (
+    <div>
+      <p>
+        지금 진행되고 있는 이벤트가 없어요 <br />
+        선물 펀딩/위시풀 중 하나를 시작해 보세요!
+      </p>
+      <p>{CTA_MESSAGE[planType]} </p>
+    </div>
+  );
 };
 
 export default ActiveEventSection;
--- src/app/Home/CreateEventSection.tsx
+++ src/app/Home/CreateEventSection.tsx
@@ -2,8 +2,29 @@
 
 import type { PlanType } from '@/types/planType';
 
-const CreateEventSection = (planType: PlanType) => {
-  return <div>{planType}</div>;
+const CREATE_CONTENT = {
+  funding: {
+    title: '선물 펀딩',
+    description: '나를 위한 생일 선물 펀딩을 직접 열기',
+  },
+  wishpool: {
+    title: '위시풀',
+    description: '생일자를 위한 위시리스트 함께 만들기',
+  },
+};
+
+const CreateEventSection = ({ planType }: { planType: PlanType }) => {
+  const { title, description } = CREATE_CONTENT[planType];
+
+  return (
+    <button>
+      <div>
+        <p>{title}</p>
+        <p>{description}</p>
+      </div>
+      <span>만들기</span>
+    </button>
+  );
 };
 
 export default CreateEventSection;
