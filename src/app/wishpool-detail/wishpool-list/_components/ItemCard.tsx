1af388a2a2970e4ead6bd82eecba0db46dbfd5ff
sunkiteo-o <hyhy020301@gmail.com>
2025-06-30 21:42:41 +0900

    style: text 중앙정렬 수정 (#25)

--- src/app/wishpool-detail/wishpool-list/_components/ItemCard.tsx
+++ src/app/wishpool-detail/wishpool-list/_components/ItemCard.tsx
@@ -13,7 +13,7 @@ const ItemCard = ({ name, item }: ItemCardProps) => {
         />
 
         <div className="mt-[0.8rem] flex items-center gap-[0.8rem]">
-          <div className="bg-button-color caption1 flex h-[2.3rem] w-[4.7rem] justify-center rounded-[4px] px-[0.8rem] py-[0.4rem] text-white">
+          <div className="bg-button-color caption1 flex h-[2.3rem] w-[4.7rem] items-center justify-center rounded-[4px] px-[0.8rem] py-[0.4rem] text-white">
             참여자
           </div>
           <div className="subtitle3">{name}</div>
