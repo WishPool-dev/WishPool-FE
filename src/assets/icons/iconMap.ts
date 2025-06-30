513221f24aa5dfa39b587eaeecc0e238f3fbc33a
봄 <saebom@Saebomui-MacBookPro.local>
2025-06-30 15:33:29 +0900

    chore: 아이콘 추가 (#18)

--- src/assets/icons/iconMap.ts
+++ src/assets/icons/iconMap.ts
@@ -2,10 +2,11 @@
 
 import BackIcon from '@/assets/icons/svg/icon_back.svg';
 import CancelIcon from '@/assets/icons/svg/icon_cancel.svg';
-import giftIcon from '@/assets/icons/svg/icon_gift.svg';
+import DotIcon from '@/assets/icons/svg/icon_dot.svg';
+import GiftIcon from '@/assets/icons/svg/icon_gift.svg';
 import HomeIcon from '@/assets/icons/svg/icon_home.svg';
 import MenuIcon from '@/assets/icons/svg/icon_menu.svg';
-import photoIcon from '@/assets/icons/svg/icon_photo.svg';
+import PhotoIcon from '@/assets/icons/svg/icon_photo.svg';
 import WishIcon from '@/assets/icons/svg/icon_wish.svg';
 
 export const iconMap = {
@@ -14,6 +15,7 @@ export const iconMap = {
   menu: MenuIcon,
   wish: WishIcon,
   cancel: CancelIcon,
-  gift: giftIcon,
-  photo: photoIcon,
+  gift: GiftIcon,
+  photo: PhotoIcon,
+  dot: DotIcon,
 } as const;
