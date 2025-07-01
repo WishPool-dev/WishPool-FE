24a3b223d64757e52c5527c30405efbbd30f3ab0
sunkiteo-o <hyhy020301@gmail.com>
2025-07-01 21:06:05 +0900

    feat: 버튼 레이아웃 추가 (#25)

--- src/app/wishpool-detail/wishpool-list/page.tsx
+++ src/app/wishpool-detail/wishpool-list/page.tsx
@@ -1,35 +1,39 @@
 import ItemCard from './_components/ItemCard';
+import WishpoolLayout from './layout';
 
 const numberOfFriends = 4;
-const numberOfGifts = 4;
 const giftItems = [
   { id: 1, name: '김이름', gift: '선물 이름' },
   { id: 2, name: '김이름', gift: '선물 이름' },
   { id: 3, name: '김이름', gift: '선물 이름' },
   { id: 4, name: '김이름', gift: '선물 이름' },
+  { id: 5, name: '김이름', gift: '선물 이름' },
+  { id: 6, name: '김이름', gift: '선물 이름' },
+  { id: 7, name: '김이름', gift: '선물 이름' },
 ];
 
 const page = () => {
   return (
-    <>
-      <div className="text-text text-left">
-        <div className="head1 py-[0.4rem]">위시풀 만들기 </div>
-        <div className="body2 pt-[0.4rem]">
-          현재까지 총 <span>{numberOfFriends}</span>명의 친구가 <br /> 생일자를
-          위한 선물 리스트를 만들어 줬어요.
+    <WishpoolLayout>
+      <div className="mt-[1.2rem]">
+        <div className="text-text text-left">
+          <div className="head1 py-[0.4rem]">위시풀 만들기 </div>
+          <div className="body2 pt-[0.4rem]">
+            현재까지 총 <span>{numberOfFriends}</span>명의 친구가 <br />
+            생일자를 위한 선물 리스트를 만들어 줬어요.
+          </div>
+        </div>
+        <div className="body2 flex flex-row justify-between pt-[2rem]">
+          <span className="text-text">선물 리스트</span>
+          <span className="text-blue-primary">{giftItems.length}개</span>
+        </div>
+        <div className="mt-[1.6rem] grid grid-cols-2 justify-items-center gap-x-[1.1rem] gap-y-[1.6rem] pb-[15rem]">
+          {giftItems.map((item) => (
+            <ItemCard key={item.id} name={item.name} item={item.gift} />
+          ))}
         </div>
       </div>
-      <div className="body2 flex flex-row justify-between pt-[2rem]">
-        <span className="text-text">선물 리스트</span>
-        <span className="text-blue-primary">{numberOfGifts}개</span>
-      </div>
-
-      <div className="mt-[1.6rem] grid grid-cols-2 justify-items-center gap-x-[1.1rem] gap-y-[1.6rem]">
-        {giftItems.map((item) => (
-          <ItemCard key={item.id} name={item.name} item={item.gift} />
-        ))}
-      </div>
-    </>
+    </WishpoolLayout>
   );
 };
 
