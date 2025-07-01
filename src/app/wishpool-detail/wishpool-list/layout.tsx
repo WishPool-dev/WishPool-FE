2268275cda9dcef458632ce60721ad616a2f1f2d
sunkiteo-o <hyhy020301@gmail.com>
2025-07-01 20:48:17 +0900

    feat: 버튼 레이아웃에 그래디언트 속성 추가 (#25)

--- src/app/wishpool-detail/wishpool-list/layout.tsx
+++ src/app/wishpool-detail/wishpool-list/layout.tsx
@@ -8,7 +8,7 @@ export default function WishpoolLayout({
   return (
     <>
       <section className="relative">{children}</section>
-      <div className="fixed right-0 bottom-[2rem] left-0 mx-auto max-w-[430px] px-[2rem]">
+      <div className="fixed right-0 bottom-0 left-0 mx-auto h-[15.7rem] max-w-[43rem] overflow-hidden bg-gradient-to-b from-white/0 to-white px-[2rem] pt-[8.6rem] pb-[2rem]">
         <Button>다음</Button>
       </div>
     </>
