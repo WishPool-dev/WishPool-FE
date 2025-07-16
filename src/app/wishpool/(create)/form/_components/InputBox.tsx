c8cc18b36a81eeff63e2d2ecf5cde3ebb178fa7d
sunkiteo-o <hyhy020301@gmail.com>
2025-07-16 21:07:43 +0900

    feat: 사용자 입력 부분 컴포넌트 제작 (#34)

---
+++ src/app/wishpool/(create)/form/_components/InputBox.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+import CalendarPicker from './CalendarPicker';
+
+interface InputBoxProps {
+  content?: string;
+  placeholder?: string;
+  maxLength?: number;
+  calendar?: boolean;
+}
+
+const InputBox = ({
+  content,
+  placeholder,
+  maxLength,
+  calendar,
+}: InputBoxProps) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (maxLength && e.target.value.length > maxLength) {
+      setInputValue(e.target.value.slice(0, maxLength));
+    } else {
+      setInputValue(e.target.value);
+    }
+  };
+  return (
+    <>
+      <div className="subtitle2 text-text mb-[0.8rem] max-w-full">
+        {content}
+      </div>
+      <div className="relative w-full">
+        <input
+          type="text"
+          value={inputValue}
+          maxLength={maxLength}
+          placeholder={placeholder}
+          onChange={handleInputChange}
+          className="body1 flex h-[5.6rem] w-full rounded-[1.2rem] border-[0.1rem] border-gray-400 px-[1.6rem] py-[0.8rem] focus:border-gray-400 focus:outline-none"
+        />
+        {maxLength && (
+          <div className="caption2 absolute right-[1.6rem] bottom-[2rem] mt-[0.4rem] text-gray-600">
+            {inputValue.length}/{maxLength}
+          </div>
+        )}
+
+        {calendar && (
+          <div className="absolute top-[1.6rem] right-[1.6rem]">
+            <CalendarPicker setValue={setInputValue} />
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default InputBox;
