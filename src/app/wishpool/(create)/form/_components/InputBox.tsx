658a625ba03505855c826c69494f236f785b548f
sunkiteo-o <hyhy020301@gmail.com>
2025-07-18 00:24:14 +0900

    feat: rounded rem 단위 px로 수정, textarea 크기 수정 (#34)

--- src/app/wishpool/(create)/form/_components/InputBox.tsx
+++ src/app/wishpool/(create)/form/_components/InputBox.tsx
@@ -28,7 +28,7 @@ const InputBox = ({
   };
   return (
     <>
-      <div className="subtitle2 text-text mb-[0.8rem] max-w-full">
+      <div className="subtitle2 text-text mb-[0.8rem] max-w-[430px]">
         {content}
       </div>
       <div className="relative w-full">
@@ -37,7 +37,8 @@ const InputBox = ({
           maxLength={maxLength}
           placeholder={placeholder}
           onChange={handleInputChange}
-          className={`body1 ${height} flex w-full rounded-[1.2rem] border-[0.1rem] border-gray-400 px-[1.6rem] py-[0.8rem] focus:border-gray-400 focus:outline-none`}
+          className={`body1 ${height} flex w-full rounded-[12px] border border-gray-400 px-[1.6rem] py-[1.5rem] focus:border-gray-400 focus:outline-none`}
+          // 피그마에는 1.6rem인데 1.5로해야 드래그가 안생김
         />
         {maxLength && (
           <div className="caption2 absolute right-[1.6rem] bottom-[2rem] mt-[0.4rem] text-gray-600">
