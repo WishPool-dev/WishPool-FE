9f43108d3fffdd2ec6044f85bdf575a9afbd7e56
Saebom <spring180@naver.com>
2025-07-31 19:10:15 +0900

    refactor: Question 컴포넌트 코드 수정 (#34)

--- src/app/wishpool/(create)/form/_components/Question.tsx
+++ src/app/wishpool/(create)/form/_components/Question.tsx
@@ -1,18 +1,16 @@
-interface QuestionProps {
+type QuestionProps = {
   required: boolean;
   question: string;
-}
+};
 
 const Question = ({ required, question }: QuestionProps) => {
+  const label = required ? '필수 입력' : '선택 입력';
+  const labelColor = required ? 'text-pink-primary' : 'text-gray-600';
+
   return (
     <>
-      {required ? (
-        <div className="text-pink-primary caption1 h-[1.8rem]">필수 입력</div>
-      ) : (
-        <div className="caption1 h-[1.8rem] text-gray-600">선택 입력</div>
-      )}
-
-      <div className="head1 text-text h-[3.2rem] pt-[0.4rem]">{question}</div>
+      <span className={`caption1 h-[1.8rem] ${labelColor}`}>{label}</span>
+      <h1 className="head1 text-text h-[3.2rem] pt-[0.4rem]">{question}</h1>
     </>
   );
 };
