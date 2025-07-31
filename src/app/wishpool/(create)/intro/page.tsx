146cbbe70f94a02918182374ebd89546700fc71f
Saebom <spring180@naver.com>
2025-07-31 18:18:17 +0900

    feat: 상수 적용 & 시맨틱 태그 수정 (#34)

--- src/app/wishpool/(create)/intro/page.tsx
+++ src/app/wishpool/(create)/intro/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 
 import { InfoNotice } from '@/app/wishpool/(create)/intro/_components/InfoNotice';
+import { INFO_NOTICE_LIST } from '@/app/wishpool/(create)/intro/_constants/infoNoticeList';
 import introEmptyBox from '@/assets/images/intro-empty-box.png';
 import Icon from '@/components/common/Icon';
 import IconButton from '@/components/common/IconButton';
@@ -11,53 +12,34 @@ const Page = () => {
   return (
     <>
       <BackHeader title="위시풀 만들기" />
-      <div className="bg-background-02 relative -mx-[2rem] -mt-[5.4rem] min-h-[calc(100vh-5.4rem)] px-[2rem] pt-[5.4rem]">
-        <div className="text-text mt-[2.8rem]">
-          <p className="head1 mb-[0.4rem]">
-            친구를 위한 선물 위시리스트, <br /> 위시풀을 함께 만들어보세요!
-          </p>
-          <p className="body1">단계별 항목에 응답해주세요.</p>
-        </div>
+      <div className="bg-background-02 -mx-[2rem] -mt-[5.4rem] h-[100vh] px-[2rem] pt-[8.2rem]">
+        <h1 className="head1 text-text mb-[0.4rem]">
+          친구를 위한 선물 위시리스트, <br /> 위시풀을 함께 만들어보세요!
+        </h1>
+        <h2 className="body1 text-text">단계별 항목에 응답해주세요.</h2>
+
         <Image
           src={introEmptyBox}
-          alt="위시풀 생성 예시 사진"
+          alt="위시풀 박스 사진"
           className="mx-auto my-[4.2rem] h-[18rem] w-[18rem] object-cover"
         />
 
-        <div className="bg-background-01 rounded-[16px] p-[2rem]">
-          <p className="text-blue-primary title3 mb-[1.2rem]">
+        <section className="bg-background-01 rounded-[16px] p-[2rem]">
+          <h3 className="text-blue-primary title3 mb-[1.2rem]">
             WishpooL만의 위시풀 만들기란?
-          </p>
-          <p className="caption2 text-gray-800">
+          </h3>
+          <p className="caption2 mb-[2rem] text-gray-800">
             위시풀은 참여자들이 생일자를 위해 각자의 선물을 제안하고, <br />
             생일자가 그 중 원하는 상품을 고르는 공동 제작 위시리스트에요.
           </p>
-          <div className="mt-[2rem]">
-            <InfoNotice
-              accordionContent={
-                <p className="caption2 text-gray-800">
-                  친구들이 제안한 선물 중에서 <br /> 생일자는 진짜 받고 싶은 걸
-                  직접 선택할 수 있어요. <br />
-                  선물받는 입장에서 더 솔직하고, 편안한 방식이에요.
-                </p>
-              }
-            >
-              생일자는 진짜 필요한 걸 고를 수 있어요.
-            </InfoNotice>
+          <div className="space-y-[1.2rem]">
+            {INFO_NOTICE_LIST.map(({ title, content }, index) => (
+              <InfoNotice key={index} accordionContent={content}>
+                {title}
+              </InfoNotice>
+            ))}
           </div>
-          <div className="mt-[1.2rem]">
-            <InfoNotice
-              accordionContent={
-                <p className="caption2 text-gray-800">
-                  친구들이 제안한 선물들이 하나의 위시리스트로 모여요. <br />
-                  위시리스트를 함께 만드는 과정도 축하의 일부가 되죠.
-                </p>
-              }
-            >
-              하나의 리스트, 여럿의 마음이 들어 있어요.
-            </InfoNotice>
-          </div>
-        </div>
+        </section>
 
         <div className="fixed right-0 bottom-[2rem] left-0 mx-auto max-w-[43rem] px-[2rem]">
           <IconButton>
