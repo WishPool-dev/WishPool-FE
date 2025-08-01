dab44887513ce94d7ed001e0c07009fe9eb4a8d5
Saebom <spring180@naver.com>
2025-08-01 17:11:46 +0900

    feat: 로그인 안되었을 때 온보딩 페이지로 redirect (#37)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-'use client';
+import { redirect } from 'next/navigation';
 
 import Home from '@/app/home/page';
 
 export default function Page() {
+  const isLoggedIn = true;
+
+  if (!isLoggedIn) {
+    redirect('/intro');
+  }
   return <Home />;
 }
