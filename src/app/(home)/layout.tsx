fb88e94d646242c39cb370f8c64382a2e69c7286
Saebom <spring180@naver.com>
2025-08-02 19:23:54 +0900

    feat: 홈 레이아웃 수정 (#39)

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,9 +4,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <HomeHeader hasMenu bgColor="white" />
-      <main className="flex flex-col items-center justify-center pt-[8.2rem]">
-        {children}
-      </main>
+      <main className="mx-[2rem] pt-[8.2rem]">{children}</main>
     </>
   );
 };
