f82a0d75761d309fcaeb4903ad3ef74e75eed903
봄 <saebom@Saebomui-MacBookPro.local>
2025-08-05 19:05:17 +0900

    feat: 분기별 Detail Footer 컴포넌트 제작 (#43)

--- src/components/wishpool/viewer/detail/DetailFooter.tsx
+++ src/components/wishpool/viewer/detail/DetailFooter.tsx
@@ -1,21 +1,44 @@
 import Button from '@/components/common/Button';
+import Icon from '@/components/common/Icon';
 
-const DetailFooter = () => {
+type DetailFooterProps = {
+  day?: number;
+  title: string;
+  content: string;
+  buttonContent: string;
+  disabled?: boolean;
+};
+
+const DetailFooter = ({
+  day,
+  title,
+  content,
+  buttonContent,
+  disabled,
+}: DetailFooterProps) => {
   return (
-    <footer className="p-[2rem flex border-t-gray-300 bg-white">
+    <footer className="flex items-center border-t-gray-300 bg-white p-[2rem]">
       <div className="grow-1 space-y-[0.4rem]">
         <div className="flex items-center gap-[0.8rem]">
-          <span className="caption1 bg-blue-6 text-blue-primary rounded-[4px] px-[0.6rem] py-[0.2rem]">
-            D-1
-          </span>
-          <span className="subtitle2 text-text">위시풀 만드는 중</span>
+          {day ? (
+            <span className="caption1 bg-blue-6 text-blue-primary rounded-[4px] px-[0.6rem] py-[0.2rem]">
+              D-{day}
+            </span>
+          ) : (
+            <Icon name="cheer" />
+          )}
+          <span className="subtitle2 text-text">{title}</span>
         </div>
-        <p className="caption2 text-gray-600">
-          선물 리스트 마감까지 1일 남았어요.
-        </p>
+        <p className="caption2 text-gray-600">{content}</p>
       </div>
       <div className="grow-1">
-        <Button textSize="sm">나도 참여하기</Button>
+        <Button
+          textSize="sm"
+          disabled={disabled}
+          backgroundColor={disabled ? 'disabled' : 'dark'}
+        >
+          {buttonContent}
+        </Button>
       </div>
     </footer>
   );
