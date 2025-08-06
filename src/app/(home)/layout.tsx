d12bebe128ec86b63808f9586326ffa0f6bcc40c
봄 <saebom@Saebomui-MacBookPro.local>
2025-08-06 15:48:21 +0900

    feat: 홈 화면 layout 수정 (#51)

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,7 +4,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <HomeHeader hasMenu bgColor="white" />
-      <main className="mx-[2rem] pt-[8.2rem]">{children}</main>
+      <main className="mx-[2rem] pt-[5.4rem]">{children}</main>
     </>
   );
 };
