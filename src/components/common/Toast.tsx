55c2d1428588ceb1837768cda2ac492a27758b32
봄 <saebom@Saebomui-MacBookPro.local>
2025-08-06 15:11:43 +0900

    feat: 타이머 기능 추가 (#51)

--- src/components/common/Toast.tsx
+++ src/components/common/Toast.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+
 const Toast = ({ children }: { children: React.ReactNode }) => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!visible) return null;
+
   return (
     <div className="z-toast fixed top-[2rem] right-0 left-0">
       <div className="mx-auto w-full max-w-[430px] px-[2rem]">
