225ffaae657d6e5ecfa1d12031bdf4913e1cff66
Saebom <spring180@naver.com>
2025-08-09 15:32:04 +0900

    chore: 케이크 아이콘 추가 (#53)

--- src/assets/icons/iconMap.ts
+++ src/assets/icons/iconMap.ts
@@ -3,6 +3,7 @@
 import BackIcon from '@/assets/icons/svg/icon_back.svg';
 import BirthdayIcon from '@/assets/icons/svg/icon_birthday.svg';
 import GiftButtonIcon from '@/assets/icons/svg/icon_button_gift.svg';
+import CakeIcon from '@/assets/icons/svg/icon_cake.svg';
 import CalendarIcon from '@/assets/icons/svg/icon_calender.svg';
 import CameraIcon from '@/assets/icons/svg/icon_camera.svg';
 import CancelIcon from '@/assets/icons/svg/icon_cancel.svg';
@@ -36,6 +37,7 @@ export const iconMap = {
   kakao: KaKaoIcon,
   more: MoreIcon,
   cheer: CheerIcon,
+  cake: CakeIcon,
 } as const;
 
 export type IconName = keyof typeof iconMap;
