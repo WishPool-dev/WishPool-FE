acc3fec874075c518df13b225e7d052b00fb7256
Saebom <spring180@naver.com>
2025-08-11 15:33:52 +0900

    feat: 위시풀 수정 페이지 layout (#53)

---
+++ src/app/wishpool/(viewer)/edit/[id]/layout.tsx
@@ -0,0 +1,11 @@
+import BackHeader from '@/components/layout/Header/BackHeader';
+
+const EditLayout = () => {
+  return (
+    <>
+      <BackHeader title="위시풀 수정" />
+    </>
+  );
+};
+
+export default EditLayout;
