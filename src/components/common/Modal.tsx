dc16cce57c9872893c141e81d84d4b7ad9af8a8d
Saebom <spring180@naver.com>
2025-08-11 14:52:53 +0900

    feat: 모달 컴포넌트 조건 추가 (#53)

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -6,9 +6,10 @@ import { createPortal } from 'react-dom';
 type ModalProps = {
   children: React.ReactNode;
   onClose?: () => void;
+  isDelete?: boolean;
 };
 
-const Modal = ({ children, onClose }: ModalProps) => {
+const Modal = ({ children, onClose, isDelete = false }: ModalProps) => {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -24,7 +25,9 @@ const Modal = ({ children, onClose }: ModalProps) => {
 
   return createPortal(
     <div
-      className="z-modal fixed inset-0 mx-auto max-w-[430px] bg-black/60"
+      className={`z-modal fixed inset-0 mx-auto max-w-[430px] ${
+        isDelete ? 'bg-black/60' : ''
+      }`}
       onClick={onClose}
     >
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
