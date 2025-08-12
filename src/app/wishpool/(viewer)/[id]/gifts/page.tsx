cd41c5d342631cf720f362db4b87bbf35f689ca0
Saebom <spring180@naver.com>
2025-08-12 14:37:50 +0900

    feat: 위시풀 리스트 빈 페이지 추가 (#53)

--- src/app/wishpool/(viewer)/[id]/gifts/page.tsx
+++ src/app/wishpool/(viewer)/[id]/gifts/page.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
+
+import giftBox from '@/assets/images/intro-empty-box.png';
 import ItemCard from '@/components/wishpool/viewer/list/ItemCard';
+
 const numberOfFriends = 4;
 const giftItems = [
   { id: 1, name: '김이름', gift: '선물 이름' },
@@ -10,6 +14,31 @@ const giftItems = [
   { id: 7, name: '김이름', gift: '선물 이름' },
 ];
 
+const GiftList = (
+  <section>
+    <div className="mt-[1.6rem] grid grid-cols-2 gap-x-[1.2rem] gap-y-[1.6rem]">
+      {giftItems.map((item) => (
+        <ItemCard key={item.id} name={item.name} item={item.gift} />
+      ))}
+    </div>
+  </section>
+);
+
+const EmptyList = (
+  <section className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-[0.4rem]">
+    <Image
+      src={giftBox}
+      alt="선물 박스 사진"
+      width={180}
+      height={180}
+      className="block"
+    />
+    <p className="title1 text-text text-center">
+      선물 리스트가 아직 도착하지 않았어요!
+    </p>
+  </section>
+);
+
 const page = () => {
   return (
     <>
@@ -18,17 +47,11 @@ const page = () => {
         <span className="text-blue-primary">홍길동</span>님을 위한 선물 리스트를
         만들어 줬어요.
       </div>
-      <section>
-        <div className="body2 flex items-center justify-between">
-          <span className="text-text">선물 리스트</span>
-          <span className="text-blue-primary">{giftItems.length}개</span>
-        </div>
-        <div className="mt-[1.6rem] grid grid-cols-2 gap-x-[1.2rem] gap-y-[1.6rem]">
-          {giftItems.map((item) => (
-            <ItemCard key={item.id} name={item.name} item={item.gift} />
-          ))}
-        </div>
-      </section>
+      <div className="body2 flex items-center justify-between">
+        <span className="text-text">선물 리스트</span>
+        <span className="text-blue-primary">{giftItems.length}개</span>
+      </div>
+      {giftItems.length > 0 ? GiftList : EmptyList}
     </>
   );
 };
