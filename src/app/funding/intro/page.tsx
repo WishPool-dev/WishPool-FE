2e9fa11f13b897635172f4821543ff79d8a8549d
Saebom <spring180@naver.com>
2025-08-14 18:22:57 +0900

    feat: 펀딩 만들기 시작 페이지 (#55)

--- src/app/funding/intro/page.tsx
+++ src/app/funding/intro/page.tsx
@@ -1,5 +1,29 @@
+import Button from '@/components/common/Button';
+
 const IntroPage = () => {
-  return <>gg</>;
+  return (
+    <>
+      <div className="text-text">
+        <h1 className="head1">
+          위시풀을 통해 <br />
+          받고 싶은 선물을 고를 수 있어요.
+        </h1>
+        <p className="body1 mt-[0.4rem]">
+          한개의 선물이 남을 때까지 원하지 않는 선물을 지워 보세요!
+        </p>
+      </div>
+      <img
+        src="/images/funding-pool.svg"
+        alt="선물 고르기 사진"
+        className="mx-auto my-[4.2rem] h-[18rem] w-[18rem]"
+      />
+      <div className="fixed inset-x-0 bottom-0">
+        <div className="mx-auto w-full max-w-[430px] p-[2rem]">
+          <Button>다음</Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default IntroPage;
