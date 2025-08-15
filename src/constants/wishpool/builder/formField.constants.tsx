477540110a7e1c67fa87fc5d1922caa80dc3dfac
sunkiteo-o <hyhy020301@gmail.com>
2025-08-15 22:13:47 +0900

    feat: FormField 모드 나눠서 구현 (#47)

---
+++ src/constants/wishpool/builder/formField.constants.tsx
@@ -0,0 +1,7 @@
+export const DEFAULT_HEIGHT = 'h-[5.6rem]';
+export const LONG_HEIGHT = 'h-[15rem]';
+
+export const formFieldHeightMap = {
+  default: DEFAULT_HEIGHT,
+  long: LONG_HEIGHT,
+} as const;
