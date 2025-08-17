ac1dbab44e931078e7486b13e2aa78ecc7095ecf
Saebom <spring180@naver.com>
2025-08-17 21:58:23 +0900

    feat: GiftCard 컴포넌트 제작 (#55)

--- src/components/funding/select/GiftCard.tsx
+++ src/components/funding/select/GiftCard.tsx
@@ -1,8 +1,19 @@
-const GiftCard = () => {
+type GiftCardProps = {
+  giftName: string;
+  giftImage: string;
+};
+
+const GiftCard = ({ giftName, giftImage }: GiftCardProps) => {
   return (
-    <div className="z-card flex flex-col items-center rounded-[16px] bg-white">
-      <img src="/images/gift-card.svg" />
-      <span className="line-clamp-2">선물 이름</span>
+    <div className="bg-background-01 flex grow-1 flex-col items-center gap-[2.4rem] rounded-[16px] p-[2.2rem]">
+      <img
+        src={giftImage}
+        alt="선물 카드 이미지"
+        className="h-[12.6rem] w-[12.6rem] rounded-[12px]"
+      />
+      <span className="subtitle2 text-text mb-[1rem] line-clamp-2 h-[4.8rem] text-center">
+        {giftName}
+      </span>
     </div>
   );
 };
