8e6d5179b669884142670b413835d738b88856fb
Saebom <spring180@naver.com>
2025-08-17 21:56:56 +0900

    feat: 선물 카드를 위한 mock data 넣기 (#55)

---
+++ src/app/funding/list/data.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    giftName: '선물 이름',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름 긴 버전 ',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름이 엄청 엄청 엄청나게 길 때를 위한 버전',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름 1',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름 2',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름 3',
+    giftImage: '/images/gift-card.svg',
+  },
+  {
+    giftName: '선물 이름 4',
+    giftImage: '/images/gift-card.svg',
+  },
+];
