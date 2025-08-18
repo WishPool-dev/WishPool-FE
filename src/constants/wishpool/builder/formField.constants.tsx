9cba3ec36a56c152a2a2df06a2d8f12df11b1c00
sunkiteo-o <hyhy020301@gmail.com>
2025-08-18 23:48:04 +0900

    feat: 폼 관련 상수, 타입 제거 (#47)

--- src/constants/wishpool/builder/formField.constants.tsx
+++
@@ -1,7 +0,0 @@
-export const DEFAULT_HEIGHT = 'h-[5.6rem]';
-export const LONG_HEIGHT = 'h-[15rem]';
-
-export const formFieldHeightMap = {
-  default: DEFAULT_HEIGHT,
-  long: LONG_HEIGHT,
-} as const;
