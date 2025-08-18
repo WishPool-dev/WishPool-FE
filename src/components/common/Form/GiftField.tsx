41b24c06facfcb22246b508d8197cb1b2f185098
sunkiteo-o <hyhy020301@gmail.com>
2025-08-19 00:47:37 +0900

    feat: linkfield 컴포넌트 이름 변경 (#47)

--- src/components/common/Form/GiftField.tsx
+++ src/components/common/Form/GiftField.tsx
@@ -2,29 +2,24 @@ import BaseInput from '@/components/common/Form/BaseInput';
 import Icon from '@/components/common/Icon';
 import { FormFieldProps } from '@/types/wishpool/builder/Form';
 
-const LinkField = ({ label, placeholder, maxLength }: FormFieldProps) => {
+const GiftField = ({ label, placeholder, maxLength }: FormFieldProps) => {
   return (
     <>
       <div className="mb-[0.8rem] flex justify-between">
         <p className="subtitle2 text-text max-w-[430px]">{label}</p>
         <button className="flex items-center justify-center" type="button">
-          <Icon
-            name="trash"
-            className="cursor-pointer"
-            width={20}
-            height={20}
-          />
+          <Icon name="trash" width={20} height={20} />
         </button>
       </div>
       <BaseInput placeholder={placeholder} maxLength={maxLength} />
       <div className="mt-[1.2rem]">
         <BaseInput
           placeholder="선물 판매 링크를 붙여넣기 해 주세요."
-          type="url"
+          // type="url"
         />
       </div>
     </>
   );
 };
 
-export default LinkField;
+export default GiftField;
