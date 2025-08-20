e4b2c7081a59173339ee437b18056debbdbfb055
sunkiteo-o <hyhy020301@gmail.com>
2025-08-20 21:37:27 +0900

    feat: 공유하는 부분만 남기고 정리 (#46)

--- src/components/common/WishpoolShareSection.tsx
+++ src/components/common/WishpoolShareSection.tsx
@@ -1,28 +1,17 @@
 'use client';
-import Image from 'next/image';
 import { useState } from 'react';
 
-import invite from '@/assets/images/invite.png';
-import share from '@/assets/images/share.png';
 import IconButton from '@/components/common/Button/IconButton';
 import Icon from '@/components/common/Icon';
 import Toast from '@/components/common/Toast';
-import { ShareTarget } from '@/types/common/shareTarget';
+import { ShareSectionType } from '@/types/common/ShareSectionType';
 
 const WISHPOOL_SHARE_CONTENT = {
-  participants: {
-    title: '생일자를 위한 위시풀이 열렸어요!',
-    description: '이제 리스트를 함께 만들 참여자들을 모집해요.',
-    imgSrc: invite,
-    imgAlt: '초대 단계 이미지',
+  invite: {
     linkTitle: '위시풀 초대장',
     linkDescription: '위시풀 만들기에 참여해 줄 친구들에게 공유해 주세요!',
   },
-  celebrant: {
-    title: '위시풀 완성!',
-    description: '이제 생일인 친구가 선물을 고를 수 있어요!',
-    imgSrc: share,
-    imgAlt: '위시풀 공유 완료 일러스트',
+  share: {
     linkTitle: '위시풀 링크',
     linkDescription: '생일자에게 위시풀 링크를 공유해 주세요!',
   },
@@ -30,17 +19,16 @@ const WISHPOOL_SHARE_CONTENT = {
 
 interface ShareComponentProps {
   linkUrl: string;
-  shareTarget: ShareTarget;
+  linkContent: ShareSectionType;
 }
 
 const WishpoolShareSection = ({
   linkUrl,
-  shareTarget,
+  linkContent,
 }: ShareComponentProps) => {
   const [toast, setToast] = useState(false);
 
-  const { title, description, imgSrc, imgAlt, linkTitle, linkDescription } =
-    WISHPOOL_SHARE_CONTENT[shareTarget];
+  const { linkTitle, linkDescription } = WISHPOOL_SHARE_CONTENT[linkContent];
 
   const handleCopyUrl = () => {
     navigator.clipboard
@@ -57,14 +45,6 @@ const WishpoolShareSection = ({
   return (
     <>
       {toast && <Toast>링크가 복사되었습니다!</Toast>}
-      <div className="mt-[2.8rem] flex flex-col items-center">
-        <h3 className="text-text head1">{title}</h3>
-        <p className="text-text body1 mt-[0.4rem]">{description}</p>
-      </div>
-      <div className="mt-[3.8rem] flex items-center justify-center">
-        <Image src={imgSrc} alt={imgAlt} width={180} height={180} />
-      </div>
-
       <div className="bg-background-02 mt-[3.6rem] h-[19.2rem] w-full rounded-[12px] p-[2rem]">
         <p className="subtitle2 text-text ml-[0.6rem]">{linkTitle}</p>
         <div className="mt-[1.1rem] flex h-[4.9rem] w-full items-center justify-between gap-[1.2rem] rounded-[12px] bg-white px-[1.2rem] py-[0.8rem]">
