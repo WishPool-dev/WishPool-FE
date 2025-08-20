af747ec87d47625a0696dff141154d906a7be81c
Saebom <spring180@naver.com>
2025-08-21 02:02:50 +0900

    feat: 피드백 페이지 제작 (#68)

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import Button from '@/components/common/Button';
+import TextField from '@/components/common/Form/TextField';
+import Rating from '@/components/common/Rating';
+import Toast from '@/components/common/Toast';
+import { PATH } from '@/constants/common/path';
+
+const FeedbackPage = () => {
+  const [openToast, setOpenToast] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    setOpenToast(true);
+    router.push(PATH.HOME);
+  };
+
+  return (
+    <section className="mt-[2.8rem] flex flex-col gap-[6.4rem]">
+      {openToast && <Toast>피드백이 전송되었습니다.</Toast>}
+      <div>
+        <p className="text-text subtitle2 mb-[1.6rem]">
+          WishpooL 서비스를 평가해 주세요.
+        </p>
+        <Rating rate={2} />
+      </div>
+
+      <div>
+        <TextField
+          label="어떤 점이 좋았나요? 또는 아쉬운 점이 있었나요?"
+          placeholder="여러분의 의견을 남겨 주세요."
+          maxLength={500}
+        />
+      </div>
+      <div className="fixed inset-x-0 bottom-0 p-[2rem]">
+        <Button type="submit" onClick={handleSubmit}>
+          보내기
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default FeedbackPage;
