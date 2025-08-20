e2aa01e4988d775e19bcf8545ef8fadef81efcdb
Saebom <spring180@naver.com>
2025-08-21 01:28:16 +0900

    feat: Rating 컴포넌트 제작 (#68)

---
+++ src/components/common/Rating.tsx
@@ -0,0 +1,21 @@
+import Icon from '@/components/common/Icon';
+
+type RatingProps = {
+  rate: number;
+};
+
+const Rating = ({ rate }: RatingProps) => {
+  return (
+    <div className="flex justify-center gap-[0.4rem]">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <Icon
+          key={i}
+          name="star"
+          className={`h-[3.2rem] w-[3.2rem] ${i < rate ? 'text-blue-2' : 'text-gray-300'}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Rating;
