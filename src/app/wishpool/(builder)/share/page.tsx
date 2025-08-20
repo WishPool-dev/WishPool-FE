e966caef407a10358c424758c0af9042cd3f83e1
sunkiteo-o <hyhy020301@gmail.com>
2025-08-20 21:38:40 +0900

    feat: WishpoolShareSection 변경에 따른 컴포넌트 수정  (#46)

--- src/app/wishpool/(builder)/share/page.tsx
+++ src/app/wishpool/(builder)/share/page.tsx
@@ -1,11 +1,31 @@
+import Image from 'next/image';
+
+import share from '@/assets/images/share.png';
 import WishpoolShareSection from '@/components/common/WishpoolShareSection';
+import { ShareSectionType } from '@/types/common/ShareSectionType';
 
 const SharePage = () => {
-  const linkurl = 'https://wishpool.store/${wishpoolId}'; // 백엔드에서 제공하는 링크로 변경 필요
-  const target = 'participants';
+  const url = 'https://wishpool.store/${wishpoolId}';
+  const content = 'share' as ShareSectionType;
+
   return (
     <>
-      <WishpoolShareSection linkUrl={linkurl} shareTarget={target} />
+      <div className="mt-[2.8rem] flex flex-col items-center">
+        <h3 className="text-text head1">위시풀 완성!</h3>
+        <p className="text-text body1 mt-[0.4rem]">
+          이제 생일인 친구가 선물을 고를 수 있어요!
+        </p>
+      </div>
+      <div className="mt-[3.8rem] flex items-center justify-center">
+        <Image
+          src={share}
+          alt="위시풀 공유 완료 일러스트"
+          width={180}
+          height={180}
+        />
+      </div>
+
+      <WishpoolShareSection linkUrl={url} linkContent={content} />
     </>
   );
 };
