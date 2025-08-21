767c6245fd45ab0057a96b313ff74c09f12cffb4
sunkiteo-o <hyhy020301@gmail.com>
2025-08-21 12:06:05 +0900

    feat: 썸네일 이미지 수정 추가 (#67)

--- src/app/wishpool/(viewer)/[id]/edit/page.tsx
+++ src/app/wishpool/(viewer)/[id]/edit/page.tsx
@@ -6,12 +6,13 @@ import Button from '@/components/common/Button';
 import BasicInputField from '@/components/common/Form/BasicInputField';
 import CalendarField from '@/components/common/Form/CalendarField';
 import TextField from '@/components/common/Form/TextField';
+import Icon from '@/components/common/Icon';
 
 const EditPage = () => {
   return (
     <>
-      <section className="mt-[5.4rem] px-[2rem]">
-        <div className="-mx-[2rem]">
+      <section className="mt-[5.4rem] px-[2rem] pb-[16.9rem]">
+        <div className="relative -mx-[2rem]">
           <Image
             src="/images/wishpool-card.svg"
             width={430}
@@ -19,26 +20,40 @@ const EditPage = () => {
             className="w-full"
             alt="위시풀 대표 이미지"
           />
+          <div className="absolute inset-0 bg-black/20"></div>
+          <div className="absolute top-1/2 left-1/2 h-[7.2rem] w-[7.2rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white/70">
+            <div className="relative flex h-full w-full flex-col items-center justify-center gap-[0.2rem]">
+              <Icon
+                name="photo"
+                width={24}
+                height={24}
+                className="text-gray-600"
+              />
+              <p className="caption1 text-gray-600">바꾸기</p>
+            </div>
+          </div>
         </div>
+
         <span className="bg-background-02 caption2 mt-[2.8rem] inline-block rounded-[4px] px-[1.2rem] py-[0.6rem] text-gray-600">
           <span className="text-blue-primary">홍길동</span>에게 보내는 위시풀
         </span>
-        <div className="mt-[3.2rem]">
+        <div className="mt-[2.8rem]">
           <BasicInputField maxLength={20} label="생일자 이름" />
         </div>
-        <div className="mt-[3.2rem]">
+        <div className="mt-[2.8rem]">
           <CalendarField label="곧 다가올 생일" />
         </div>
+        <div className="bg-background-02 w-max-[430px] -mx-[2rem] my-[3.2rem] h-[0.8rem]"></div>
         <div className="mt-[4rem]">
           <TextField label="위시풀 제목" maxLength={200} />
         </div>
-        <div className="mt-[3.2rem]">
+        <div className="mt-[2.8rem]">
           <CalendarField tag="참여자" label="선물 리스트 마감일" />
         </div>
-        <div className="fixed right-0 bottom-[2rem] left-0 mx-auto max-w-[43rem] px-[2rem]">
-          <Button>저장하기</Button>
-        </div>
       </section>
+      <div className="fixed right-0 bottom-[2rem] left-0 mx-auto max-w-[43rem] px-[2rem]">
+        <Button>저장하기</Button>
+      </div>
     </>
   );
 };
