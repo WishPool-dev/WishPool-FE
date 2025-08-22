8ffbe91ff387e6f925724cb63796ba934d33abc9
sunkiteo-o <hyhy020301@gmail.com>
2025-08-22 12:37:13 +0900

    feat: 위시풀 참여 intro 페이지 (#72)

---
+++ src/app/wishpool/join/([id])/intro/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import Button from '@/components/common/Button';
+import InfoBox from '@/components/common/InfoBox/InfoBox';
+import { PATH } from '@/constants/common/path';
+
+const IntroPage = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="text-text">
+        <h1 className="head1">
+          위시풀을 통해 <br />
+          <span className="text-blue-primary">홍길동</span>님에게 주고 싶은
+          선물을 제안해요.
+        </h1>
+        <p className="body1 mt-[0.4rem]">단계별 항목에 응답해주세요.</p>
+      </div>
+      <img
+        src="/images/funding-pool.svg"
+        alt="선물 고르기 사진"
+        className="mx-auto my-[4.2rem] h-[18rem] w-[18rem]"
+      />
+      <div className="fixed inset-x-0 bottom-0">
+        <div className="mx-auto w-full max-w-[430px] p-[2rem]">
+          <Button
+            onClick={() => {
+              router.push(PATH.FUNDING_SELECT);
+            }}
+          >
+            시작
+          </Button>
+        </div>
+      </div>
+      <InfoBox />
+    </>
+  );
+};
+
+export default IntroPage;
