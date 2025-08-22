4b1962992aee725886c0138451914001fa56c107
Saebom <spring180@naver.com>
2025-08-22 22:08:20 +0900

    feat: 컴포넌트 연결 (#74)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-
-import Icon from '@/components/common/Icon';
+import MyPageItem from '@/components/mypage/MyPageItem';
+import MyPageList from '@/components/mypage/MyPageList';
+import { PATH } from '@/constants/common/path';
 
 const MyPage = () => {
   return (
@@ -13,37 +13,19 @@ const MyPage = () => {
         />
         <span className="text-text head1">User Name</span>
       </div>
-      <div className="px-[2rem] py-[3.2rem]">
-        <h3 className="text-text subtitle2 mb-[1.6rem]">지난 이벤트</h3>
-        <div className="bg-background-01 flex flex-col rounded-[12px]">
-          <Link
-            className="flex items-center justify-between px-[1.6rem] py-[1.4rem]"
-            href="history"
-          >
-            <span className="text-text title1">위시풀 기록</span>
-            <Icon name="next" />
-          </Link>
-          <Link className="px-[1.6rem] py-[1.4rem]" href="history">
-            <span className="title1 text-gray-300">
-              선물 펀딩 기록(준비 중)
-            </span>
-          </Link>
-        </div>
-      </div>
-      <div className="mb-[1.6rem] px-[2rem] py-[3.2rem]">
-        <h3 className="text-text subtitle2">관리</h3>
-        <div className="bg-background-01 flex flex-col rounded-[12px]">
-          <Link
-            className="flex items-center justify-between px-[1.6rem] py-[1.4rem]"
-            href="history"
-          >
-            <span className="text-text title1">평가/피드백 하기</span>
-            <Icon name="next" />
-          </Link>
-          <Link className="px-[1.6rem] py-[1.4rem]" href="history">
-            <span className="title1 text-text">로그아웃</span>
-          </Link>
-        </div>
+
+      <div className="flex flex-col gap-[3.2rem] px-[2rem] py-[3.2rem]">
+        <MyPageList title="지난 이벤트">
+          <MyPageItem page="위시풀 기록" link={PATH.WISHPOOL_HISTORY} />
+          <MyPageItem page="선물 펀딩 기록(준비 중)" link="" disabled />
+        </MyPageList>
+
+        <MyPageList title="관리">
+          <MyPageItem page="평가/피드백 하기" link={PATH.FEEDBACK} />
+          <button className="text-text title1 px-[1.6rem] py-[1.4rem] text-left">
+            로그아웃
+          </button>
+        </MyPageList>
       </div>
     </div>
   );
--- src/components/mypage/MyPageItem.tsx
+++ src/components/mypage/MyPageItem.tsx
@@ -5,16 +5,19 @@ import Icon from '@/components/common/Icon';
 type MyPageItemProps = {
   page: string;
   link: string;
+  disabled?: boolean;
 };
 
-const MyPageItem = ({ page, link }: MyPageItemProps) => {
+const MyPageItem = ({ page, link, disabled }: MyPageItemProps) => {
   return (
     <li>
       <Link
         className="flex items-center justify-between px-[1.6rem] py-[1.4rem]"
         href={link}
       >
-        <span className="text-text title1">{page}</span>
+        <span className={`title1 ${disabled ? 'text-gray-300' : 'text-text'}`}>
+          {page}
+        </span>
         <Icon name="next" />
       </Link>
     </li>
