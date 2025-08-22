e13be2d7e5d00216a218ce19c2106a9eca5c6246
Saebom <spring180@naver.com>
2025-08-23 04:13:16 +0900

    feat: 선물 선택 섹션 레이아웃 전체 수정 (#82)

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -19,30 +19,32 @@ const FeedbackPage = () => {
   };
 
   return (
-    <section className="mt-[2.8rem] flex flex-col gap-[6.4rem]">
+    <>
       {openToast && <Toast>평가를 보냈어요. 소중한 의견에 감사드려요!</Toast>}
-      <div>
-        <p className="text-text subtitle2 mb-[1.6rem]">
-          WishpooL 서비스를 평가해 주세요.
-        </p>
-        <Rating rate={2} />
-      </div>
-
-      <div>
-        <TextField
-          label="어떤 점이 좋았나요? 또는 아쉬운 점이 있었나요?"
-          placeholder="여러분의 의견을 남겨 주세요."
-          maxLength={500}
-        />
-      </div>
-      <div className="fixed inset-x-0 bottom-0">
-        <div className="mx-auto w-full max-w-[430px] p-[2rem]">
-          <Button type="submit" onClick={handleSubmit}>
-            보내기
-          </Button>
+      <section className="mt-[2.8rem] flex flex-col gap-[6.4rem]">
+        <div>
+          <p className="text-text subtitle2 mb-[1.6rem]">
+            WishpooL 서비스를 평가해 주세요.
+          </p>
+          <Rating rate={2} />
         </div>
-      </div>
-    </section>
+
+        <div>
+          <TextField
+            label="어떤 점이 좋았나요? 또는 아쉬운 점이 있었나요?"
+            placeholder="여러분의 의견을 남겨 주세요."
+            maxLength={500}
+          />
+        </div>
+        <div className="fixed inset-x-0 bottom-0">
+          <div className="mx-auto w-full max-w-[430px] p-[2rem]">
+            <Button type="submit" onClick={handleSubmit}>
+              보내기
+            </Button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/app/funding/intro/layout.tsx
+++ src/app/funding/intro/layout.tsx
@@ -3,8 +3,8 @@ import HomeHeader from '@/components/layout/Header/HomeHeader';
 const IntroLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <HomeHeader />
-      <main className="bg-background-02 h-[100vh] px-[2rem] pt-[8.2rem]">
+      <HomeHeader bgColor="background02" />
+      <main className="bg-background-02 px-[2rem] pt-[8.2rem] pb-[10rem]">
         {children}
       </main>
     </>
