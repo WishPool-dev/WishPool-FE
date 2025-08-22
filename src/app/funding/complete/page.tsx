263a02437a8297050ed8114f6ea9c827b255eff7
Saebom <spring180@naver.com>
2025-08-22 18:24:04 +0900

    feat: 하단 버튼 컨테이너 제작 (#65)

--- src/app/funding/complete/page.tsx
+++ src/app/funding/complete/page.tsx
@@ -1,4 +1,5 @@
 import { data } from '@/app/funding/complete/data';
+import Button from '@/components/common/Button';
 import GiftCard from '@/components/funding/list/GiftCard';
 const CompletePage = () => {
   return (
@@ -16,6 +17,17 @@ const CompletePage = () => {
           />
         ))}
       </section>
+      <div className="fixed right-0 bottom-0 left-0 mx-auto max-w-[430px] border-t border-gray-300 bg-white px-[2rem] pb-[2rem]">
+        <p className="caption1 my-[1.6rem] text-center text-gray-600">
+          이 선물로 결정할까요?
+        </p>
+        <div className="flex justify-between gap-[1.3rem]">
+          <Button textColor="black" backgroundColor="light" textSize="sm">
+            다시 고르기
+          </Button>
+          <Button textSize="sm">완료하기</Button>
+        </div>
+      </div>
     </div>
   );
 };
