ba5fbc2afc618c8cc18991666c929f9475ca59a7
Saebom <spring180@naver.com>
2025-08-22 20:05:38 +0900

    feat: complete 페이지 헤더 변경 (#65)

--- src/app/funding/complete/layout.tsx
+++ src/app/funding/complete/layout.tsx
@@ -1,9 +1,9 @@
-import TextHeader from '@/components/layout/Header/TextHeader';
+import HomeHeader from '@/components/layout/Header/HomeHeader';
 
 const CompleteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <TextHeader title="친구에게 보내는 위시풀" bgColor="white" />
+      <HomeHeader bgColor="white" />
       <main className="bg-background-01 px-[2rem] pt-[5.4rem] pb-[2rem]">
         {children}
       </main>
