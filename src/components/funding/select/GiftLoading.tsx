5f695fd48071392ce4a11bfa49137b29d62a4ec3
Saebom <spring180@naver.com>
2025-08-22 19:45:34 +0900

    feat: GiftLoading 컴포넌트 생성 (#65)

---
+++ src/components/funding/select/GiftLoading.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { data } from '@/app/funding/list/data';
+import GiftCard from '@/components/funding/list/GiftCard';
+
+const GiftLoading = () => {
+  const duration = 5;
+
+  return (
+    <section className="relative my-[20rem] overflow-hidden px-[2rem]">
+      <div className="pointer-events-none absolute" />
+      <motion.div
+        className="flex w-max gap-[1.2rem] will-change-transform"
+        animate={{ x: ['0%', '-50%'] }}
+        transition={{ duration, ease: 'linear', repeat: Infinity }}
+        aria-hidden
+      >
+        {data.map(({ giftId, giftName, giftImage }) => (
+          <GiftCard
+            key={giftId}
+            giftId={giftId}
+            giftName={giftName}
+            giftImage={giftImage}
+          />
+        ))}
+      </motion.div>
+    </section>
+  );
+};
+
+export default GiftLoading;
