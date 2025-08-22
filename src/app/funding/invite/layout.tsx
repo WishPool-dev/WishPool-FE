b51a7a4b6ec7cc93f9224914aa679cda83829474
Saebom <spring180@naver.com>
2025-08-23 01:42:09 +0900

    feat: 초대장 페이지 제작 (#78)

--- src/app/funding/invite/layout.tsx
+++ src/app/funding/invite/layout.tsx
@@ -1,5 +1,14 @@
-const layout = () => {
-  return <div>layout</div>;
+import HomeHeader from '@/components/layout/Header/HomeHeader';
+
+const InviteLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <HomeHeader />
+      <main className="from-Background-02 to-blue-5 h-[100vh] bg-gradient-to-b px-[2rem] pt-[5.4rem]">
+        {children}
+      </main>
+    </>
+  );
 };
 
-export default layout;
+export default InviteLayout;
