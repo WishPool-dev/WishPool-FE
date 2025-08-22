36842855d1ab7f6ec0d256705a418f36130c369b
Saebom <spring180@naver.com>
2025-08-22 18:54:17 +0900

    feat: 선물 선택 완료 페이지 제작 (#65)

--- src/app/funding/complete/layout.tsx
+++ src/app/funding/complete/layout.tsx
@@ -1,10 +1,10 @@
-import HomeHeader from '@/components/layout/Header/HomeHeader';
+import TextHeader from '@/components/layout/Header/TextHeader';
 
 const CompleteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <HomeHeader bgColor="background02" />
-      <main className="bg-background-02 px-[2rem] pt-[5.4rem] pb-[2rem]">
+      <TextHeader title="친구에게 보내는 위시풀" bgColor="white" />
+      <main className="bg-background-01 px-[2rem] pt-[5.4rem] pb-[2rem]">
         {children}
       </main>
     </>
--- src/app/funding/complete/page.tsx
+++ src/app/funding/complete/page.tsx
@@ -1,5 +1,25 @@
+import Image from 'next/image';
+
+import GiftBoxImage from '@/assets/images/wish-in-box.png';
+import FeedbackBox from '@/components/common/FeedbackBox';
+
 const CompletePage = () => {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col items-center pt-[5.6rem]">
+      <h1 className="text-text head1 mb-[0.4rem]">
+        위시풀 선물 고르기가 끝났어요!
+      </h1>
+      <h2 className="text-text body1">이 결과를 친구들에게 알려줄게요!</h2>
+      <Image
+        src={GiftBoxImage}
+        alt="선물 상자 이미지"
+        width={180}
+        height={180}
+        className="mt-[3.8rem] mb-[8.4rem]"
+      />
+      <FeedbackBox />
+    </div>
+  );
 };
 
 export default CompletePage;
