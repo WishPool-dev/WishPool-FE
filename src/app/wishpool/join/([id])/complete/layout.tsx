46eb73c87dbd40c0058dcd4b3b80cdbbf6e0ac96
sunkiteo-o <hyhy020301@gmail.com>
2025-08-23 01:59:43 +0900

    feat: complete layout 수정 (#75)

--- src/app/wishpool/join/([id])/complete/layout.tsx
+++ src/app/wishpool/join/([id])/complete/layout.tsx
@@ -19,7 +19,7 @@ const PreviewLayout = ({ children }: { children: React.ReactNode }) => {
         title="홍길동에게 보내는 위시풀"
         onClick={handleClose}
       />
-      <main className="mx-[2rem] pt-[8.6rem]">{children}</main>
+      <main className="mx-[2rem] pt-[5.4rem]">{children}</main>
     </>
   );
 };
