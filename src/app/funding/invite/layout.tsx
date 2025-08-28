8274c3fe42746e22641e5b70b2b6b4924fd588c4
봄 <saebom@Saebomui-MacBookPro.local>
2025-08-28 17:13:54 +0900

    style: invite 페이지 헤더 투명색으로 변경 (#87)

--- src/app/funding/invite/layout.tsx
+++ src/app/funding/invite/layout.tsx
@@ -3,7 +3,7 @@ import HomeHeader from '@/components/layout/Header/HomeHeader';
 const InviteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <HomeHeader bgColor="background02" />
+      <HomeHeader />
       <main className="from-Background-02 to-blue-5 bg-gradient-to-b px-[2rem] pt-[5.4rem] pb-[15rem]">
         {children}
       </main>
