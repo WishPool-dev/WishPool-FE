e31e242a607071bbbea45df63e5c27ec39d06089
sunkiteo-o <hyhy020301@gmail.com>
2025-09-01 22:10:26 +0900

    feat: 상단 패딩값 추가 (#90)

--- src/app/wishpool/(builder)/intro/layout.tsx
+++ src/app/wishpool/(builder)/intro/layout.tsx
@@ -6,7 +6,7 @@ const IntroLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <BackHeader title="위시풀 만들기" bgColor="background02" />
-      <main className="bg-background-02 h-[100vh] pt-[8.4rem] pb-[10rem]">
+      <main className="bg-background-02 pt-[5.4rem] pb-[30rem]">
         {children}
       </main>
     </>
