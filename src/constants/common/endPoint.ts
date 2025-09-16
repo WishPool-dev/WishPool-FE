a21517f566709957b59c6e6f506a147b536c5f53
Saebom <spring180@naver.com>
2025-09-16 23:10:28 +0900

    feat: 엔드 포인트 상수 설정 (#94)

---
+++ src/constants/common/endPoint.ts
@@ -0,0 +1,10 @@
+export const END_POINT = {
+  CHECKED: 'api',
+  WISHPOOL: 'wishpools',
+  JOIN: 'join',
+  GIFTS: 'gifts',
+  GUEST: 'guests',
+  CELEBRANT: 'celebrant',
+  SELECT: 'selections',
+  AUTH: 'oauth2',
+};
