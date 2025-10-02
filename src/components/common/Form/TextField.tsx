a468feda0239c461c662527ee6620a6345bd94e5
sunkiteo-o <hyhy020301@gmail.com>
2025-10-02 12:30:32 +0900

    feat:  textField 라벨 분리 및 세션 스토리지 추가(#92)

--- src/components/common/Form/TextField.tsx
+++ src/components/common/Form/TextField.tsx
@@ -1,26 +1,41 @@
-import { useState } from 'react';
+'use client';
+import { useState, useEffect } from 'react';
 
 import { FormFieldProps } from '@/types/wishpool/builder/Form';
 
-const TextField = ({ label, maxLength, placeholder }: FormFieldProps) => {
-  const [inputValue, setInputValue] = useState('');
-  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInputValue(e.target.value);
+const TextField = ({
+  name,
+  placeholder,
+  maxLength,
+  defaultValue,
+}: FormFieldProps) => {
+  const limit = typeof maxLength === 'number' ? maxLength : undefined;
+
+  const [val, setVal] = useState(defaultValue ?? '');
+
+  useEffect(() => {
+    setVal(String(defaultValue));
+  }, [defaultValue]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const text = limit ? e.target.value.slice(0, limit) : e.target.value;
+    setVal(text);
+    sessionStorage.setItem(`wishpool_${name}`, text);
   };
   return (
     <>
-      <p className="subtitle2 text-text mb-[0.8rem] max-w-[430px]">{label}</p>
       <div className="relative w-full">
         <textarea
+          name={name}
           maxLength={maxLength}
-          value={inputValue}
-          onChange={handleInputChange}
+          value={val}
+          onChange={handleChange}
           placeholder={placeholder}
-          className="body1 flex h-[15rem] w-full resize-none rounded-[12px] border border-gray-400 px-[1.6rem] py-[1.5rem] placeholder:text-gray-400 focus:border-gray-400 focus:outline-none"
+          className="body1 flex h-[15rem] w-full resize-none rounded-[12px] border border-gray-400 px-[1.6rem] py-[1.5rem] pb-[3rem] whitespace-pre-line placeholder:text-gray-400 focus:border-gray-400 focus:outline-none"
         />
-        {maxLength && (
+        {limit && (
           <div className="caption2 absolute right-[1.6rem] bottom-[2rem] mt-[0.4rem] text-gray-600">
-            {inputValue.length}/{maxLength}
+            {val.length}/{limit}
           </div>
         )}
       </div>
