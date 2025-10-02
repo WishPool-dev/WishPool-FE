ab4dcd0ed8bfefa07212bb09bf557efda32a809f
sunkiteo-o <hyhy020301@gmail.com>
2025-10-02 12:18:23 +0900

    feat: step 이동 버튼 컴포넌트 수정 (#92)

--- src/components/wishpool/builder/create/ButtonContainer.tsx
+++ src/components/wishpool/builder/create/ButtonContainer.tsx
@@ -2,43 +2,19 @@
 import { useRouter } from 'next/navigation';
 
 import Button from '@/components/common/Button';
-import { PATH } from '@/constants/common/path';
-import { STEP_PATHS, Step } from '@/constants/wishpool/create/steps';
-
-type ButtonContainerProps = {
-  currentStep: Step;
-  isNextDisabled?: boolean;
-};
-
-const MIN_STEP: Step = 1;
-const MAX_STEP: Step = 4;
 
 const ButtonContainer = ({
-  currentStep,
   isNextDisabled,
-}: ButtonContainerProps) => {
+  next,
+}: {
+  isNextDisabled: boolean;
+  next: string;
+}) => {
   const router = useRouter();
 
-  const handlePrev = () => {
-    if (currentStep === MIN_STEP) {
-      router.push(PATH.WISHPOOL_INTRO);
-      return;
-    }
-
-    const prevStep = (currentStep - 1) as Step;
-    router.push(STEP_PATHS[prevStep]);
-  };
-
   const handleNext = () => {
-    if (currentStep === MAX_STEP) {
-      router.push(PATH.WISHPOOL_PREVIEW);
-      return;
-    }
-
-    const nextStep = (currentStep + 1) as Step;
-    router.push(STEP_PATHS[nextStep]);
+    router.push(next);
   };
-
   return (
     <div className="fixed right-0 bottom-[2rem] left-0 mx-auto max-w-[43rem]">
       <div className="flex justify-between gap-[1rem] px-[2rem]">
@@ -46,11 +22,12 @@ const ButtonContainer = ({
           textColor="black"
           backgroundColor="light"
           textSize="sm"
-          onClick={handlePrev}
+          onClick={() => history.back()}
         >
           이전
         </Button>
         <Button
+          type="button"
           textColor="white"
           backgroundColor={isNextDisabled ? 'disabled' : 'dark'}
           textSize="sm"
