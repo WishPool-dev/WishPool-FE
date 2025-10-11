add9f0ad96d0f8617ad0693d131aaf42e92d05a9
sunkiteo-o <hyhy020301@gmail.com>
2025-10-11 09:18:51 +0900

    feat: WishpoolDetailResponse 타입 추가 (#110)

--- src/api/domain/edit/hooks.ts
+++ src/api/domain/edit/hooks.ts
@@ -1,5 +1,8 @@
 import { useMutation } from '@tanstack/react-query';
 
+import { QUERY_KEY } from '@/constants/common/queryKey';
+import { queryClient } from '@/lib/queryClient';
+
 import { patchWisipool } from './index';
 import { WishpoolCreateType } from '../form/types';
 
@@ -12,5 +15,10 @@ export const usePatchWishpool = () => {
       wishpoolId: number;
       payload: WishpoolCreateType;
     }) => patchWisipool(wishpoolId, payload),
+    onSuccess: (wishpoolId) => {
+      queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.WISHPOOL_DETAIL(wishpoolId),
+      });
+    },
   });
 };
--- src/app/wishpool/(viewer)/[id]/edit/page.tsx
+++ src/app/wishpool/(viewer)/[id]/edit/page.tsx
@@ -2,8 +2,13 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import {
+  useGetWishpoolDetail,
+  useGetWishpoolImage,
+} from '@/api/domain/detail/hooks';
+import { usePatchWishpool } from '@/api/domain/edit/hooks';
 import WishpoolCardImage from '@/assets/images/wishpool-card.png';
 import Button from '@/components/common/Button';
 import Calendar from '@/components/common/Calendar';
@@ -12,40 +17,96 @@ import TextField from '@/components/common/Form/TextField';
 import Icon from '@/components/common/Icon';
 import Toast from '@/components/common/Toast';
 import UserTag from '@/components/common/UserTag';
+import { useGetWishpoolId } from '@/hooks/common/useGetWishpoolId';
 
 const EditPage = () => {
-  const [openToast, setOpenToast] = useState(false);
   const router = useRouter();
 
-  const onClickHandler = () => {
+  const [openToast, setOpenToast] = useState(false);
+
+  // 폼 상태관리
+  const [formData, setFormData] = useState({
+    celebrant: '',
+    birthDay: '',
+    endDate: '',
+    description: '',
+    imageKey: '',
+  });
+
+  // 폼 초기 상태 및 이미지 받아오기
+  const wishpoolId = useGetWishpoolId();
+  const { data: detail } = useGetWishpoolDetail(wishpoolId);
+
+  const { data: wishpoolImage } = useGetWishpoolImage(detail?.imageKey ?? '');
+
+  const displayImageUrl = wishpoolImage?.key
+    ? wishpoolImage?.key
+    : WishpoolCardImage;
+
+  useEffect(() => {
+    if (detail) {
+      setFormData({
+        celebrant: detail.celebrant,
+        birthDay: detail.birthDay,
+        endDate: detail.endDate,
+        description: detail.description,
+        imageKey: detail.imageKey,
+      });
+
+      console.log(detail);
+    }
+  }, [detail]);
+
+  const handleImageChange = () => {
     //TODO: 이미지 업로드 기능 추가
-  };
-  const handleSubmit = () => {
-    setOpenToast(true);
-    router.back();
+    // usePostWishpoolImage 로 key 받아서 formdata.imageKey에 넣기
   };
 
-  const handleInputChange = () => {
-    // TODO: 입력값 수정 기능 추가
+  // 버튼 누르면 수정 완료
+  const updateMutation = usePatchWishpool();
+
+  const handleSubmit = async () => {
+    if (!wishpoolId) return;
+
+    const payload = {
+      celebrant: formData.celebrant,
+      birthDay: formData.birthDay,
+      endDate: formData.endDate,
+      description: formData.description,
+      imageKey: formData.imageKey,
+    };
+
+    try {
+      await updateMutation.mutateAsync({ wishpoolId, payload });
+      setOpenToast(true);
+      setTimeout(() => router.back(), 1500);
+    } catch (err) {
+      console.error(err);
+      alert('위시풀 수정에 실패했습니다.');
+    }
+  };
+
+  const handleInputChange = (name: string, value: string) => {
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   return (
     <>
       {openToast && <Toast>위시풀이 수정되었어요.</Toast>}
 
-      <div className="relative">
+      <div className="relative h-[22.2rem]">
         <Image
-          src={WishpoolCardImage}
+          src={displayImageUrl}
           width={430}
           height={240}
-          className="w-full"
+          className="h-full w-full object-cover"
           alt="위시풀 대표 이미지"
         />
         <div className="absolute inset-0 bg-black/20" />
 
         <button
           type="button"
-          onClick={onClickHandler}
+          onClick={handleImageChange}
           className="absolute top-1/2 left-1/2 h-[7.2rem] w-[7.2rem] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white/70"
           aria-label="이미지 바꾸기"
         >
@@ -63,7 +124,8 @@ const EditPage = () => {
 
       <div className="mb-[20rem] p-[2rem]">
         <span className="bg-background-02 caption2 inline-block rounded-[4px] px-[1.2rem] py-[0.6rem] text-gray-600">
-          <span className="text-blue-primary">홍길동</span>에게 보내는 위시풀
+          <span className="text-blue-primary">{formData.celebrant}</span>에게
+          보내는 위시풀
         </span>
         <div className="mt-[2.8rem]">
           <div className="subtitle2 text-text mb-[0.8rem] max-w-[430px]">
@@ -71,6 +133,7 @@ const EditPage = () => {
           </div>
           <BaseInput
             name="celebrant"
+            value={formData.celebrant}
             placeholder="생일자의 이름을 알려 주세요."
             maxLength={20}
             onChange={handleInputChange}
@@ -80,7 +143,11 @@ const EditPage = () => {
           <p className="subtitle2 text-text mb-[0.8rem] max-w-[430px]">
             곧 다가올 생일
           </p>
-          <Calendar name="birthDay" />
+          <Calendar
+            name="birthDay"
+            value={formData.birthDay}
+            onChange={handleInputChange}
+          />
         </div>
 
         <hr className="bg-background-02 -mx-[2rem] my-[3.2rem] h-[0.8rem] border-0" />
@@ -91,6 +158,7 @@ const EditPage = () => {
           </p>
           <TextField
             name="description"
+            value={formData.description}
             placeholder={`참여자가 이 소개글을 볼 수 있어요. 
 생일자에게는 보이지 않아요.`}
             maxLength={200}
@@ -105,7 +173,11 @@ const EditPage = () => {
             </p>
           </div>
 
-          <Calendar name="endDate" />
+          <Calendar
+            name="endDate"
+            value={formData.endDate}
+            onChange={handleInputChange}
+          />
         </div>
       </div>
 
