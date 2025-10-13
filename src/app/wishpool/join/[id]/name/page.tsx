b27d6063abcf5a09fbc456d18402697fbc780118
sunkiteo-o <hyhy020301@gmail.com>
2025-10-13 22:41:48 +0900

    feat: name 페이지 동적 라우팅 수정 및 참여자 이름 받기 (#117)

--- src/app/wishpool/join/[id]/name/page.tsx
+++ src/app/wishpool/join/[id]/name/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 import Button from '@/components/common/Button';
 import BaseInput from '@/components/common/Form/BaseInput';
 import Question from '@/components/common/Form/Question';
 import { PATH } from '@/constants/common/path';
+
 const NamePage = () => {
   const router = useRouter();
-  const handleInputChange = () => {};
+  const [name, setName] = useState({ participant: '' });
+  const STORAGE_KEY = 'wishpool_participant';
+
+  const wishpoolId = Number(sessionStorage.getItem('wishpoolId'));
+
+  useEffect(() => {
+    const initialParticipant = sessionStorage.getItem(STORAGE_KEY) || '';
+    setName({
+      participant: initialParticipant,
+    });
+  }, []);
+
+  const handleInputChange = (name: string, value: string) => {
+    setName({ participant: value });
+  };
+
+  const isNextDisabled = name.participant.trim() === '';
+
   return (
     <>
       <Question required={true} question="참여자 본인의 이름을 알려주세요." />
@@ -16,33 +35,34 @@ const NamePage = () => {
           본인 이름
         </div>
         <BaseInput
-          name="celebrant"
+          name="participant"
           placeholder="본인의 이름을 알려 주세요."
           maxLength={20}
           onChange={handleInputChange}
+          value={name.participant}
         />
       </div>
-      <div className="fixed inset-x-0 bottom-0 mx-auto w-full max-w-[430px] flex-col items-center justify-center p-[2rem]">
-        <div className="flex justify-between gap-[1.3rem]">
-          <Button
-            textColor="black"
-            backgroundColor="light"
-            textSize="sm"
-            onClick={() => {
-              router.push(PATH.JOIN_GIFTS);
-            }}
-          >
-            이전
-          </Button>
-          <Button
-            textSize="sm"
-            onClick={() => {
-              router.push(PATH.JOIN_ADD);
-            }}
-          >
-            다음
-          </Button>
-        </div>
+
+      <div className="fixed inset-x-0 bottom-0 flex w-full justify-between gap-[1.3rem] p-[2rem]">
+        <Button
+          textColor="black"
+          backgroundColor="light"
+          textSize="sm"
+          onClick={() => {
+            router.push(PATH.JOIN_GIFTS(wishpoolId));
+          }}
+        >
+          이전
+        </Button>
+        <Button
+          textSize="sm"
+          onClick={() => {
+            router.push(PATH.JOIN_ADD(wishpoolId));
+          }}
+          disabled={isNextDisabled}
+        >
+          다음
+        </Button>
       </div>
     </>
   );
