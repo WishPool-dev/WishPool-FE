71108b7348e1e891cb2d1f77b289acf43477707c
sunkiteo-o <hyhy020301@gmail.com>
2025-10-13 22:34:02 +0900

    feat: 대표자 위시풀 참여 api 연결 (#117)

---
+++ src/api/domain/join/index.ts
@@ -0,0 +1,12 @@
+import { axiosInstance } from '@/api/axiosInstance';
+import { END_POINT } from '@/constants/common/endPoint';
+
+import { WishpoolJoinRequest } from './types';
+
+export const postOwnerGifts = async (payload: WishpoolJoinRequest) => {
+  const res = await axiosInstance.post(
+    `/${END_POINT.API}/${END_POINT.WISHPOOLS}/${END_POINT.JOIN}`,
+    payload,
+  );
+  return res.data;
+};
