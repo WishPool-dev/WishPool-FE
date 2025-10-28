c67bb3a5f7cec4fb81cc07ea957e9b6e78abc6f8
sunkiteo-o <hyhy020301@gmail.com>
2025-10-28 22:45:53 +0900

    feat: 위시풀 참여 시 참여자 이름 제거 (#120)

--- src/app/wishpool/join/[id]/intro/page.tsx
+++ src/app/wishpool/join/[id]/intro/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import { useGetWishpoolDetail } from '@/api/domain/detail/hooks';
 import PickPoolImage from '@/assets/images/pick-pool.png';
 import Button from '@/components/common/Button';
 import InfoBox from '@/components/common/InfoBox/InfoBox';
@@ -12,16 +11,11 @@ import { useGetWishpoolId } from '@/hooks/common/useGetWishpoolId';
 const IntroPage = () => {
   const router = useRouter();
   const wishpoolId = useGetWishpoolId();
-  const { data: wishpoolData } = useGetWishpoolDetail(wishpoolId);
 
   return (
     <>
       <div className="text-text">
-        <h1 className="head1">
-          위시풀을 통해 <br />
-          <span className="text-blue-primary">{wishpoolData?.celebrant}</span>
-          님에게 주고 싶은 선물을 제안해요.
-        </h1>
+        <h1 className="head1">위시풀을 통해 주고 싶은 선물을 제안해요.</h1>
         <p className="body1 mt-[0.4rem]">단계별 항목에 응답해주세요.</p>
       </div>
       <Image
