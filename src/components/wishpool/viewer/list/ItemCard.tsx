fec9bcad3c9d6724adafa99687fbcfa8406a8278
sunkiteo-o <hyhy020301@gmail.com>
2026-01-21 17:49:29 +0900

    feat: 선물리스트 페이지에 이미지 추가 (#149)

--- src/components/wishpool/viewer/list/ItemCard.tsx
+++ src/components/wishpool/viewer/list/ItemCard.tsx
@@ -3,22 +3,27 @@ import Link from 'next/link';
 
 import GiftCardImage from '@/assets/images/gift-card.png';
 import UserTag from '@/components/common/UserTag';
-
 type ItemCardProps = {
   guest: string;
   itemName: string;
   itemUrl: string;
+  imageUrl?: string;
 };
 
-const ItemCard = ({ guest, itemName, itemUrl }: ItemCardProps) => {
+const ItemCard = ({ guest, itemName, itemUrl, imageUrl }: ItemCardProps) => {
   return (
     <>
       <Link href={itemUrl} className="flex grow-1 flex-col">
         <Image
-          src={GiftCardImage}
-          alt="프로필 이미지"
-          sizes="100vw"
-          className="rounded-[12px] object-cover"
+          src={
+            imageUrl
+              ? `${process.env.NEXT_PUBLIC_WISHPOOL_IMAGE_BASE_URL}/${imageUrl}`
+              : GiftCardImage
+          }
+          alt="선물 이미지"
+          width={155}
+          height={155}
+          className="h-[15.5rem] w-[15.5rem] rounded-[12px] object-cover"
         />
 
         <div className="mt-[0.8rem] flex items-center gap-[0.8rem]">
