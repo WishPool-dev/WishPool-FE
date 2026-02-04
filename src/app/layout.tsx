f23b38c651799368e4a7341c18d066ff28c8df25
sunkiteo-o <hyhy020301@gmail.com>
2026-02-04 19:12:25 +0900

    feat: ms-clarity 코드 추가(#181)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 
 import '@/styles/globals.css';
+import Script from 'next/script';
+
 import KakaoInitializer from '@/components/common/Button/KakaoButton/KakaoInitializer';
 import ScrollToTop from '@/components/common/ScrollToTop';
 import ReactQueryProvider from '@/components/ReactQueryProvider';
@@ -21,6 +23,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          id="ms-clarity"
+          type="text/javascript"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vbwq3wj5tb");
+          `}
+        </Script>
+      </head>
       <body className={`${suite.className} bg-gray-100`}>
         <div className="mx-auto min-h-screen w-full max-w-[430px] bg-white shadow-md">
           <ScrollToTop />
