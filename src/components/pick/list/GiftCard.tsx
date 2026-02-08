9b09ad1f05af4130fb704da7ddaccbaf8b8f0bcf
Saebom <spring180@naver.com>
2026-02-08 22:28:53 +0900

    feat: 선물에 링크 연결 (#181)

--- src/components/pick/list/GiftCard.tsx
+++ src/components/pick/list/GiftCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { useGetWishpoolImage } from '@/api/domain/detail/hooks';
 import GiftCardImage from '@/assets/images/gift-card.png';
@@ -14,6 +15,7 @@ const GiftCard = ({
   size = 'small',
   giftId,
   itemName,
+  itemUrl,
   imageUrl,
 }: GiftCardProps) => {
   const isSmall = size === 'small';
@@ -23,28 +25,30 @@ const GiftCard = ({
     imageUrl && imageData && imageData.key ? imageData.key : GiftCardImage;
 
   return (
-    <div
-      className={`bg-background-01 flex grow-1 flex-col items-center gap-[2.4rem] rounded-[16px] ${isSmall ? 'p-[2.2rem]' : 'p-[6.4rem]'}`}
-    >
+    <Link href={itemUrl}>
       <div
-        className={`relative overflow-hidden rounded-[12px] ${
-          isSmall ? 'h-[12.6rem] w-[12.6rem]' : 'h-[17.1rem] w-[17.1rem]'
-        }`}
+        className={`bg-background-01 flex grow-1 flex-col items-center gap-[2.4rem] rounded-[16px] ${isSmall ? 'p-[2.2rem]' : 'p-[6.4rem]'}`}
       >
-        <Image
-          src={finalSrc}
-          alt={`선물 카드 이미지 - ${giftId}`}
-          fill
-          className="object-cover"
-        />
-      </div>
+        <div
+          className={`relative overflow-hidden rounded-[12px] ${
+            isSmall ? 'h-[12.6rem] w-[12.6rem]' : 'h-[17.1rem] w-[17.1rem]'
+          }`}
+        >
+          <Image
+            src={finalSrc}
+            alt={`선물 카드 이미지 - ${giftId}`}
+            fill
+            className="object-cover"
+          />
+        </div>
 
-      <span
-        className={`subtitle2 text-text line-clamp-2 h-[4.8rem] text-center ${isSmall ? 'w-[12.6rem]' : ''}`}
-      >
-        {itemName}
-      </span>
-    </div>
+        <span
+          className={`subtitle2 text-text line-clamp-2 h-[4.8rem] text-center ${isSmall ? 'w-[12.6rem]' : ''}`}
+        >
+          {itemName}
+        </span>
+      </div>
+    </Link>
   );
 };
 
